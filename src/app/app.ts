a0762c5c8425d32f7d7631690fb6c44eb9564a68
wbiker <wolfbiker@mailbox.org>
2026-02-23 12:29:36 +0100

    Add angular material

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
+import {MatTableModule} from '@angular/material/table';
 import { RouterOutlet } from '@angular/router';
+import { StoryTable } from './story-table/story-table';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, StoryTable, MatTableModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
